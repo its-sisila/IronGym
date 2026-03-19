c6e29cb93b189b98de3262d2d5c4b6e61fd37c26
Sisila Dhevasirinarayana <sisila8400@gmail.com>
2026-03-19 21:09:18 +0530

    feat: integrate AboutSection into home page #4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturesSection from "@/components/FeaturesSection";
+import AboutSection from "@/components/AboutSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <FeaturesSection />
+      <AboutSection />
     </main>
   );
 }
