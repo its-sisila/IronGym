92c4647569a47776eb3998c8ce09e6a03b3d056f
Sisila Dhevasirinarayana <sisila8400@gmail.com>
2026-03-19 22:06:39 +0530

    feat: integrate ContactSection into home page #6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import FeaturesSection from "@/components/FeaturesSection";
 import AboutSection from "@/components/AboutSection";
 import TrainersSection from "@/components/TrainersSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
+import ContactSection from "@/components/ContactSection";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <AboutSection />
       <TrainersSection />
       <TestimonialsSection />
+      <ContactSection />
     </main>
   );
 }
